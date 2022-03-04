47ce867284a649990805d2251ec76989a7c9e8d6
Adair Locks Grassi <adairlocksgrassi@gmail.com>
2022-03-03 23:42:16 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+
+
+namespace ClientesApi2.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options)
+            : base(options)
+        { 
+        }
+        public DbSet<Clientes> Clientes { get; set; } = null!;
+
+    }
+}
